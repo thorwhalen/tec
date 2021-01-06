c46955194b047e278a97c3e3480b16e2516d5d5d
Thor Whalen <thorwhalen1@gmail.com>
2021-01-05 17:45:42 -0800

    file_contents_to_short_description

--- tec/__init__.py
+++ tec/__init__.py
@@ -1,16 +1,26 @@
 from tec.peek import print_source, print_signature
+
 from tec.modules import (
     loaded_module_from_dotpath_and_filepath,
     second_party_names,
     filepath_to_dotpath,
     get_imported_module_paths
 )
+
 from tec.pip_packaging import (
     create_github_repo,
     get_last_pypi_version_number,
     format_str_vals_of_dict,
     ujoin
 )
+
 from tec.packages import (
     print_top_level_diagnosis
+)
+
+from tec.stores import (
+    file_contents_to_short_description,
+    find_short_description_for_pkg,
+    PkgReader,
+    PkgFilesReader
 )
--- tec/stores.py
+++ tec/stores.py
@@ -37,12 +37,16 @@ commented_header_re = re.compile('(?<=""")\s?.+')
 triple_quotes_re = re.compile('"""$|\'\'\'$')
 
 
+def file_contents_to_short_description(file_contents: str):
+    m = commented_header_re.search(file_contents)
+    t = m.group(0).strip()
+    t = triple_quotes_re.sub('', t)
+    return t
+
+
 def find_short_description_for_pkg(s):
     """Generator of (pkg_name, short_description) pairs (using the header comments of init files as description)"""
     for k, v in s.items():
         if k.endswith('__init__.py') and (v.startswith('"""') or v.startswith("'''")):
             k = k[:-(len('__init__.py') + 1)]
-            m = commented_header_re.search(v)
-            t = m.group(0).strip()
-            t = triple_quotes_re.sub('', t)
-            yield k, t
+            yield k, file_contents_to_short_description(v)
