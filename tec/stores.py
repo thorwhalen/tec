68ee77c8b89dbc5e06290f7c6201fc4b5b147009
Thor Whalen <thorwhalen1@gmail.com>
2021-01-19 09:11:30 -0800

    enhanced file_contents_to_short_description

--- tec/stores.py
+++ tec/stores.py
@@ -33,15 +33,38 @@ class PkgReader(DirCollection, KvReader):
         return PkgFilesReader(os.path.join(self.rootdir, k))
 
 
-commented_header_re = re.compile('(?<=""")\s?.+')
-triple_quotes_re = re.compile('"""$|\'\'\'$')
+commented_header_re = re.compile('("""|\'\'\')\s?.+')
+triple_quotes_re = re.compile('"""|\'\'\'')
+triple_quotes_ending_re = re.compile('"""$|\'\'\'$')
 
 
-def file_contents_to_short_description(file_contents: str):
-    m = commented_header_re.search(file_contents)
-    t = m.group(0).strip()
-    t = triple_quotes_re.sub('', t)
-    return t
+def _clean_str(string):
+    return triple_quotes_re.sub('', string.strip())
+
+
+def file_contents_to_short_description(file_contents: str, dflt=None, max_lines=4):
+    lines = file_contents.split('\n')
+    quotes_line_idx = next(
+        (i for i, line in enumerate(lines[:max_lines])
+         if triple_quotes_re.match(line)),
+        None)
+    if quotes_line_idx is not None:
+        i = quotes_line_idx
+        first_line_of_description = lines[i][3:].strip()
+        if first_line_of_description:
+            return _clean_str(first_line_of_description)
+        i = min(len(lines), quotes_line_idx + 1)
+        first_line_of_description = lines[i].strip()
+        if first_line_of_description:
+            return _clean_str(first_line_of_description)
+    return dflt
+    # m = commented_header_re.match(file_contents)
+    # if m:
+    #     t = m.group(0)[3:].strip()
+    #     t = triple_quotes_ending_re.sub('', t)
+    #     return t
+    # else:
+    #     return dflt
 
 
 def find_short_description_for_pkg(s):
